874fc976f40eb015f10dfeaf539f9eec1b925dee
Yaakoub tair <yaakoubt05@gmail.com>
2025-11-14 09:25:41 +0100

    fix : nombre de collection in home page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,7 @@ menuBtn.addEventListener("click", () => {
     menuMobile.classList.toggle("hidden");
 });
 lucide.createIcons();
+
+const el = document.getElementById("collectionsCount");
+const collections = JSON.parse(localStorage.getItem("cards_collections") || "[]");
+el.textContent = collections.length;
